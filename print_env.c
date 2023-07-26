63518b44e3e2704c7884ee7d8be5997090cee3ca
richard miruka onsare <richardonsare15@gmail.com>
2023-07-27 01:25:16 +0300

    correction'

--- print_env.c
+++ print_env.c
@@ -1,80 +1,81 @@
 #include "main_unique.h"
 
 /**
- * print_env - prints the environment variables
+ * print_env - Prints the environment variables.
  *
- * @shell_data: data relevant.
+ * @shell_data: Pointer to the shell_data_t struct
  * Return: 1 on success.
  */
 int print_env(shell_data_t *shell_data)
 {
-	int i, j;
+    int i, j;
 
-	for (i = 0; shell_data->shell_env[i]; i++)
-	{
-		for (j = 0; shell_data->shell_env[i][j]; j++)
-			;
+    for (i = 0; shell_data->shell_env[i]; i++)
+    {
+        for (j = 0; shell_data->shell_env[i][j]; j++)
+            ;
 
-		write(STDOUT_FILENO, shell_data->shell_env[i], j);
-		write(STDOUT_FILENO, "\n", 1);
-	}
+        write(STDOUT_FILENO, shell_data->shell_env[i], j);
+        write(STDOUT_FILENO, "\n", 1);
+    }
 
-	shell_data->last_status = 0;
-	return (1);
+    shell_data->last_status = 0;
+    return (1);
 }
 
 /**
- * _getenv - get an environment variable
- * @name: name of the environment variable
- * @environment: environment variable
+ * _getenv - Get an environment variable.
+ * @name: Name of the environment variable.
+ * @environment: Environment variable.
  *
- * Return: value of the environment variable if found.
+ * Return: Value of the environment variable if found.
  * In other case, returns NULL.
  */
 char *_getenv(const char *name, char **environment)
 {
-	char *ptr_env;
-	int i, mov;
+    char *ptr_env;
+    int i, mov;
 
-	/* Initialize ptr_env value */
-	ptr_env = NULL;
-	mov = 0;
-	/* Compare all environment variables */
-	/* environ is declared in the header file */
-	for (i = 0; environment[i]; i++)
-	{
-		/* If name and env are equal */
-		mov = cmp_env_name(environment[i], name);
-		if (mov)
-		{
-			ptr_env = environment[i];
-			break;
-		}
-	}
+    /* Initialize ptr_env value */
+    ptr_env = NULL;
+    mov = 0;
 
-	return (ptr_env + mov);
+    /* Compare all environment variables */
+    /* environ is declared in the header file */
+    for (i = 0; environment[i]; i++)
+    {
+        /* If name and env are equal */
+        mov = cmp_env_name(environment[i], name);
+        if (mov)
+        {
+            ptr_env = environment[i];
+            break;
+        }
+    }
+
+    return (ptr_env + mov);
 }
 
 /**
- * cmp_env_name - compares environment variables names
+ * cmp_env_name - Compares environment variables names
  * with the name passed.
- * @nenv: name of the environment variable
- * @name: name passed
+ * @nenv: Name of the environment variable.
+ * @name: Name passed.
  *
  * Return: 0 if they are not equal. Another value if they are.
  */
 int cmp_env_name(const char *nenv, const char *name)
 {
-	int i;
+    int i;
 
-	for (i = 0; nenv[i] != '='; i++)
-	{
-		if (nenv[i] != name[i])
-		{
-			return (0);
-		}
-	}
+    for (i = 0; nenv[i] != '='; i++)
+    {
+        if (nenv[i] != name[i])
+        {
+            return (0);
+        }
+    }
 
-	return (i + 1);
+    return (i + 1);
 }
 
