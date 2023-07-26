586ff6a69b537bfc5f55ec4350bb54a5e44728bf
richard miruka onsare <richardonsare15@gmail.com>
2023-07-27 00:41:09 +0300

    correction

--- aux_string.c
+++ aux_string.c
@@ -10,14 +10,18 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
+    unsigned int i = 0;
 
-	for (; *(s + i) != '\0'; i++)
-		if (*(s + i) == c)
-			return (s + i);
-	if (*(s + i) == c)
-		return (s + i);
-	return ('\0');
+    for (; *(s + i) != '\0'; i++)
+    {
+        if (*(s + i) == c)
+            return (s + i);
+    }
+
+    if (*(s + i) == c)
+        return (s + i);
+
+    return ('\0');
 }
 
 /**
@@ -29,23 +33,25 @@ char *_strchr(char *s, char c)
  */
 int _strspn(char *s, char *accept)
 {
-	int i, j, bool;
+    int i, j, bool;
 
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		bool = 1;
-		for (j = 0; *(accept + j) != '\0'; j++)
-		{
-			if (*(s + i) == *(accept + j))
-			{
-				bool = 0;
-				break;
-			}
-		}
-		if (bool == 1)
-			break;
-	}
-	return (i);
+    for (i = 0; *(s + i) != '\0'; i++)
+    {
+        bool = 1;
+        for (j = 0; *(accept + j) != '\0'; j++)
+        {
+            if (*(s + i) == *(accept + j))
+            {
+                bool = 0;
+                break;
+            }
+        }
+
+        if (bool == 1)
+            break;
+    }
+
+    return (i);
 }
 
 /**
@@ -58,16 +64,17 @@ int _strspn(char *s, char *accept)
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+    int i;
 
-	for (i = 0; s1[i] == s2[i] && s1[i]; i++)
-		;
+    for (i = 0; s1[i] == s2[i] && s1[i]; i++)
+        ;
 
-	if (s1[i] > s2[i])
-		return (1);
-	if (s1[i] < s2[i])
-		return (-1);
-	return (0);
+    if (s1[i] > s2[i])
+        return (1);
+    if (s1[i] < s2[i])
+        return (-1);
+
+    return (0);
 }
 
 /**
@@ -79,20 +86,20 @@ int _strcmp(char *s1, char *s2)
  */
 char *_strcat(char *dest, const char *src)
 {
-	int i;
-	int j;
+    int i;
+    int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
+    for (i = 0; dest[i] != '\0'; i++)
+        ;
 
-	for (j = 0; src[j] != '\0'; j++)
-	{
-		dest[i] = src[j];
-		i++;
-	}
+    for (j = 0; src[j] != '\0'; j++)
+    {
+        dest[i] = src[j];
+        i++;
+    }
 
-	dest[i] = '\0';
-	return (dest);
+    dest[i] = '\0';
+    return (dest);
 }
 
 /**
@@ -104,13 +111,15 @@ char *_strcat(char *dest, const char *src)
  */
 char *_strcpy(char *dest, char *src)
 {
-	size_t a;
+    size_t a;
 
-	for (a = 0; src[a] != '\0'; a++)
-	{
-		dest[a] = src[a];
-	}
-	dest[a] = '\0';
+    for (a = 0; src[a] != '\0'; a++)
+    {
+        dest[a] = src[a];
+    }
 
-	return (dest);
+    dest[a] = '\0';
+
+    return (dest);
 }
+
