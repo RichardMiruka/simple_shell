0c5adbdf8bf4bb30559cf3b4279ad9608ca97a48
richard miruka onsare <richardonsare15@gmail.com>
2023-07-27 00:34:02 +0300

    file correction

--- aux_str_reverse.c
+++ aux_str_reverse.c
@@ -10,27 +10,28 @@
  */
 void reverse_string(char *s)
 {
-	int count = 0, i, j;
-	char *str, temp;
+    int count = 0, i, j;
+    char *str, temp;
 
-	/* Calculate the length of the input string */
-	while (count >= 0)
-	{
-		if (s[count] == '\0')
-			break;
-		count++;
-	}
-	str = s;
+    /* Calculate the length of the input string */
+    while (count >= 0)
+    {
+        if (s[count] == '\0')
+            break;
+        count++;
+    }
+    str = s;
 
-	/* Use two pointers to swap characters from both ends */
-	for (i = 0; i < (count - 1); i++)
-	{
-		for (j = i + 1; j > 0; j--)
-		{
-			/* Swap characters at positions j and j - 1 */
-			temp = *(str + j);
-			*(str + j) = *(str + (j - 1));
-			*(str + (j - 1)) = temp;
-		}
-	}
+    /* Use two pointers to swap characters from both ends */
+    for (i = 0; i < (count - 1); i++)
+    {
+        for (j = i + 1; j > 0; j--)
+        {
+            /* Swap characters at positions j and j - 1 */
+            temp = *(str + j);
+            *(str + j) = *(str + (j - 1));
+            *(str + (j - 1)) = temp;
+        }
+    }
 }
+
