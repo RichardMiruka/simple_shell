fbe02f7c70fc3554ad7ec3e312daabd20fd26e2a
richard miruka onsare <richardonsare15@gmail.com>
2023-07-26 23:18:01 +0300

    proper handling of each type of command within the shell

--- exec_line_func.c
+++ exec_line_func.c
@@ -1,3 +1,27 @@
+#include "main_unique.h"
+
+/**
+ * exec_line - Finds builtins and commands and executes them.
+ *
+ * @datash: Pointer to the shell_data_t struct (args).
+ * Return: 1 on success, -1 on error.
+ */
+int exec_line(shell_data_t *datash)
+{
+    int (*builtin)(shell_data_t *datash);
+
+    if (datash->arguments[0] == NULL)
+        return (1);
+
+    // Check if the command is a builtin
+    builtin = get_builtin_command(datash->arguments[0]);
+
+    if (builtin != NULL)
+        return (builtin(datash));
+
+    // Execute the command as an external command
+    return (execute_command(datash));
+}
 
 
 
