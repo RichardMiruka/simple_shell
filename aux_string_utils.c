f4c2944f859e6f50afa81c50c2e5e3261f505c46
richard miruka onsare <richardonsare15@gmail.com>
2023-07-27 00:39:10 +0300

    correction

--- aux_string_utils.c
+++ aux_string_utils.c
@@ -8,14 +8,14 @@
  */
 int _isdigit(const char *s)
 {
-	unsigned int i;
+    unsigned int i;
 
-	for (i = 0; s[i]; i++)
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-	}
-	return (1);
+    for (i = 0; s[i]; i++)
+    {
+        if (s[i] < '0' || s[i] > '9')
+            return (0);
+    }
+    return (1);
 }
 
 /**
@@ -27,46 +27,46 @@ int _isdigit(const char *s)
  */
 char *_strtok(char str[], const char *delim)
 {
-	static char *splitted, *str_end;
-	char *str_start;
-	unsigned int i, bool;
+    static char *splitted, *str_end;
+    char *str_start;
+    unsigned int i, bool;
 
-	if (str != NULL)
-	{
-		if (cmp_chars(str, delim))
-			return (NULL);
-		splitted = str; /* Store first address */
-		i = _strlen(str);
-		str_end = &str[i]; /* Store last address */
-	}
-	str_start = splitted;
-	if (str_start == str_end) /* Reached the end */
-		return (NULL);
+    if (str != NULL)
+    {
+        if (cmp_chars(str, delim))
+            return (NULL);
+        splitted = str; /* Store first address */
+        i = _strlen(str);
+        str_end = &str[i]; /* Store last address */
+    }
+    str_start = splitted;
+    if (str_start == str_end) /* Reached the end */
+        return (NULL);
 
-	for (bool = 0; *splitted; splitted++)
-	{
-		/* Breaking loop finding the next token */
-		if (splitted != str_start)
-			if (*splitted && *(splitted - 1) == '\0')
-				break;
-		/* Replacing delimiter for null char */
-		for (i = 0; delim[i]; i++)
-		{
-			if (*splitted == delim[i])
-			{
-				*splitted = '\0';
-				if (splitted == str_start)
-					str_start++;
-				break;
-			}
-		}
+    for (bool = 0; *splitted; splitted++)
+    {
+        /* Breaking loop finding the next token */
+        if (splitted != str_start)
+            if (*splitted && *(splitted - 1) == '\0')
+                break;
+        /* Replacing delimiter for null char */
+        for (i = 0; delim[i]; i++)
+        {
+            if (*splitted == delim[i])
+            {
+                *splitted = '\0';
+                if (splitted == str_start)
+                    str_start++;
+                break;
+            }
+        }
 
-		if (bool == 0 && *splitted) /* Str != Delim */
-			bool = 1;
-	}
-	if (bool == 0) /* Str == Delim */
-		return (NULL);
-	return (str_start);
+        if (bool == 0 && *splitted) /* Str != Delim */
+            bool = 1;
+    }
+    if (bool == 0) /* Str == Delim */
+        return (NULL);
+    return (str_start);
 }
 
 /**
@@ -76,12 +76,12 @@ char *_strtok(char str[], const char *delim)
  */
 int _strlen(const char *s)
 {
-	int len;
+    int len;
 
-	for (len = 0; s[len] != '\0'; len++)
-	{
-	}
-	return (len);
+    for (len = 0; s[len] != '\0'; len++)
+    {
+    }
+    return (len);
 }
 
 /**
@@ -93,22 +93,22 @@ int _strlen(const char *s)
  */
 int cmp_chars(char str[], const char *delim)
 {
-	unsigned int i, j, k;
+    unsigned int i, j, k;
 
-	for (i = 0, k = 0; str[i]; i++)
-	{
-		for (j = 0; delim[j]; j++)
-		{
-			if (str[i] == delim[j])
-			{
-				k++;
-				break;
-			}
-		}
-	}
-	if (i == k)
-		return (1);
-	return (0);
+    for (i = 0, k = 0; str[i]; i++)
+    {
+        for (j = 0; delim[j]; j++)
+        {
+            if (str[i] == delim[j])
+            {
+                k++;
+                break;
+            }
+        }
+    }
+    if (i == k)
+        return (1);
+    return (0);
 }
 
 /**
@@ -118,15 +118,15 @@ int cmp_chars(char str[], const char *delim)
  */
 char *_strdup(const char *s)
 {
-	char *new;
-	size_t len;
+    char *new;
+    size_t len;
 
-	len = _strlen(s);
-	new = malloc(sizeof(char) * (len + 1));
-	if (new == NULL)
-		return (NULL);
-	_memcpy(new, s, len + 1);
-	return (new);
+    len = _strlen(s);
+    new = malloc(sizeof(char) * (len + 1));
+    if (new == NULL)
+        return (NULL);
+    _memcpy(new, s, len + 1);
+    return (new);
 }
 
 /**
@@ -139,10 +139,10 @@ char *_strdup(const char *s)
  */
 void _memcpy(void *dest, const void *src, unsigned int n)
 {
-	char *char_dest = (char *)dest;
-	const char *char_src = (const char *)src;
-	unsigned int i;
+    char *char_dest = (char *)dest;
+    const char *char_src = (const char *)src;
+    unsigned int i;
 
-	for (i = 0; i < n; i++)
-		char_dest[i] = char_src[i];
+    for (i = 0; i < n; i++)
+        char_dest[i] = char_src[i];
 }
