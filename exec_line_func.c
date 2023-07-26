5614e2f3eee85ad2f0242c88b46bfe32ea039c82
richard miruka onsare <richardonsare15@gmail.com>
2023-07-27 01:06:38 +0300

    code correction

--- exec_line_func.c
+++ exec_line_func.c
@@ -11,17 +11,18 @@ int exec_line(shell_data_t *datash)
     int (*builtin)(shell_data_t *datash);
 
     if (datash->arguments[0] == NULL)
-        return (1);
+        return 1;
 
     // Check if the command is a builtin
     builtin = get_builtin_command(datash->arguments[0]);
 
     if (builtin != NULL)
-        return (builtin(datash));
+        return builtin(datash);
 
     // Execute the command as an external command
-    return (execute_command(datash));
+    return execute_command(datash);
 }
+
 
 
 
