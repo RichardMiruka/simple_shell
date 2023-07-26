efbd6a9d4d3d63d83c0e457ac255aabe166a2ac0
richard miruka onsare <richardonsare15@gmail.com>
2023-07-27 00:59:33 +0300

    corrected code

--- error_handler.c
+++ error_handler.c
@@ -9,28 +9,27 @@
  */
 int error_handler(shell_data_t *shell_data, int evaluation)
 {
-	char *error_message;
+	char *error_message = NULL;
 
 	switch (evaluation)
 	{
-		case -1:
-			error_message = error_environment_variable(shell_data);
-			break;
-		case 126:
-			error_message = error_permission(shell_data->arguments[0]);
-			break;
-		case 127:
-			error_message = error_command_not_found(shell_data);
-			break;
-		case 2:
-			if (_strcmp("exit", shell_data->arguments[0]) == 0)
-				error_message = error_exit_shell(shell_data);
-			else if (_strcmp("cd", shell_data->arguments[0]) == 0)
-				error_message = error_get_cd(shell_data);
-			break;
-		default:
-			error_message = NULL;
-			break;
+	case -1:
+		error_message = error_environment_variable(shell_data);
+		break;
+	case 126:
+		error_message = error_permission(shell_data->arguments[0]);
+		break;
+	case 127:
+		error_message = error_command_not_found(shell_data);
+		break;
+	case 2:
+		if (_strcmp("exit", shell_data->arguments[0]) == 0)
+			error_message = error_exit_shell(shell_data);
+		else if (_strcmp("cd", shell_data->arguments[0]) == 0)
+			error_message = error_get_cd(shell_data);
+		break;
+	default:
+		break;
 	}
 
 	if (error_message)
@@ -42,3 +41,4 @@ int error_handler(shell_data_t *shell_data, int evaluation)
 	shell_data->last_status = evaluation;
 	return (evaluation);
 }
+
