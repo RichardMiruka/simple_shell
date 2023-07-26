68da4b532f4e4b6d5c564f59efc35e8571d19242
richard miruka onsare <richardonsare15@gmail.com>
2023-07-27 01:01:49 +0300

    correction

--- error_handling_2.c
+++ error_handling_2.c
@@ -2,69 +2,62 @@
 
 /**
  * error_path_126 - error message for path and failure denied permission.
- * @datash: data relevant (counter, arguments).
+ * @datash: Pointer to the shell_data_t struct.
  *
  * Return: The error string.
  */
 char *error_path_126(shell_data_t *datash)
 {
-    int length;
-    char *ver_str;
-    char *error;
+	int length;
+	char *ver_str;
+	char *error;
 
-    ver_str = aux_itoa(datash->line_counter);
-    length = _strlen(datash->arguments[0]) + _strlen(ver_str);
-    length += _strlen(datash->arguments[0]) + 24;
-    error = malloc(sizeof(char) * (length + 1));
-    if (error == 0)
-    {
-        free(error);
-        free(ver_str);
-        return (NULL);
-    }
-    _strcpy(error, datash->arguments[0]);
-    _strcat(error, ": ");
-    _strcat(error, ver_str);
-    _strcat(error, ": ");
-    _strcat(error, datash->arguments[0]);
-    _strcat(error, ": Permission denied\n");
-    _strcat(error, "\0");
-    free(ver_str);
-    return (error);
+	ver_str = aux_itoa(datash->line_counter);
+	length = _strlen(datash->arguments[0]) + _strlen(ver_str) + 24;
+	error = malloc(sizeof(char) * (length + 1));
+	if (error == NULL)
+	{
+		free(ver_str);
+		return (NULL);
+	}
+	_strcpy(error, datash->arguments[0]);
+	_strcat(error, ": ");
+	_strcat(error, ver_str);
+	_strcat(error, ": ");
+	_strcat(error, datash->arguments[0]);
+	_strcat(error, ": Permission denied\n");
+	free(ver_str);
+	return (error);
 }
 
 /**
  * error_env - error message for env in get_env.
- * @datash: data relevant (counter, arguments)
- * Return: error message.
+ * @datash: Pointer to the shell_data_t struct.
+ * Return: The error message.
  */
 char *error_env(shell_data_t *datash)
 {
-    int length;
-    char *error;
-    char *ver_str;
-    char *msg;
+	int length;
+	char *error;
+	char *ver_str;
+	const char *msg = ": Unable to add/remove from environment\n";
 
-    ver_str = aux_itoa(datash->line_counter);
-    msg = ": Unable to add/remove from environment\n";
-    length = _strlen(datash->arguments[0]) + _strlen(ver_str);
-    length += _strlen(datash->arguments[0]) + _strlen(msg) + 4;
-    error = malloc(sizeof(char) * (length + 1));
-    if (error == 0)
-    {
-        free(error);
-        free(ver_str);
-        return (NULL);
-    }
+	ver_str = aux_itoa(datash->line_counter);
+	length = _strlen(datash->arguments[0]) + _strlen(ver_str) + _strlen(msg) + 4;
+	error = malloc(sizeof(char) * (length + 1));
+	if (error == NULL)
+	{
+		free(ver_str);
+		return (NULL);
+	}
 
-    _strcpy(error, datash->arguments[0]);
-    _strcat(error, ": ");
-    _strcat(error, ver_str);
-    _strcat(error, ": ");
-    _strcat(error, datash->arguments[0]);
-    _strcat(error, msg);
-    _strcat(error, "\0");
-    free(ver_str);
+	_strcpy(error, datash->arguments[0]);
+	_strcat(error, ": ");
+	_strcat(error, ver_str);
+	_strcat(error, ": ");
+	_strcat(error, datash->arguments[0]);
+	_strcat(error, msg);
+	free(ver_str);
 
-    return (error);
+	return (error);
 }
