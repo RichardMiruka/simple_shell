6aae3ed35bdcf9460b8ffbfda59f893b129f9593
richard miruka onsare <richardonsare15@gmail.com>
2023-07-28 23:31:23 +0300

    Reads a line of input from stdin

--- read_lines.c
+++ read_lines.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * unique_read_line - Reads the input string from stdin using getline.
+ * read_line - Reads a line of input from stdin.
  *
- * @i_eof: Pointer to store the return value of getline function (EOF status).
- * Return: Input string.
+ * @i_eof: Pointer to an integer to store the return value of getline function.
+ *
+ * Return: The input string read from stdin.
  */
-char *unique_read_line(int *i_eof)
+char *read_line(int *i_eof)
 {
-    char *input = NULL;
-    size_t bufsize = 0;
+	char *input = NULL;
+	size_t bufsize = 0;
 
-    *i_eof = getline(&input, &bufsize, stdin);
+	*i_eof = getline(&input, &bufsize, stdin);
 
-    return (input);
+	return (input);
 }
 
 
