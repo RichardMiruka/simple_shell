520f7c6bd703689ce91d73fea0f43ba31a70f11d
richard miruka onsare <richardonsare15@gmail.com>
2023-07-28 23:39:24 +0300

    Main loop of the shell program

--- shell_looping.c
+++ shell_looping.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * shell_loop - Main loop of the shell.
+ * shell_loop - Main loop of the shell program.
  *
- * @shell_data: Data relevant to the shell's runtime.
- * Return: No return.
+ * @datash: Pointer to the data structure containing relevant data (av, input, args).
+ *
+ * Return: No return value.
  */
-void shell_loop(shell_data_t *shell_data)
+void shell_loop(data_shell *datash)
 {
 	int loop, i_eof;
 	char *input;
@@ -14,65 +15,66 @@ void shell_loop(shell_data_t *shell_data)
 	loop = 1;
 	while (loop == 1)
 	{
-		write(STDOUT_FILENO, "^-^ ", 4);
-		input = unique_read_line(&i_eof);
+		write(STDIN_FILENO, "^-^ ", 4); /* Print the prompt symbol to stdout */
+		input = read_line(&i_eof); /* Read a line of input from stdin */
 		if (i_eof != -1)
 		{
-			input = remove_comments(input);
+			input = without_comment(input); /* Remove comments from the input */
 			if (input == NULL)
-				continue;
+				continue; /* Continue the loop if the input is empty after removing comments */
 
-			if (check_syntax_error(shell_data, input) == 1)
+			if (check_syntax_error(datash, input) == 1)
 			{
-				shell_data->last_status = 2;
+				datash->status = 2; /* Set the status to 2 if there is a syntax error */
 				free(input);
-				continue;
+				continue; /* Continue the loop to read the next input */
 			}
-			input = replace_variable(input, shell_data);
-			loop = split_commands(shell_data, input);
-			shell_data->line_counter += 1;
-			free(input);
+			input = rep_var(input, datash); /* Replace variables in the input */
+			loop = split_commands(datash, input); /* Split and execute commands from the input */
+			datash->counter += 1; /* Increment the line counter */
+			free(input); /* Free the memory allocated for the input */
 		}
 		else
 		{
-			loop = 0;
-			free(input);
+			loop = 0; /* Exit the loop if the end-of-file is reached (i_eof = -1) */
+			free(input); /* Free the memory allocated for the input */
 		}
 	}
 }
 
 /**
- * remove_comments - Deletes comments from the input.
+ * without_comment - Remove comments from the input.
  *
- * @input: Input string.
- * Return: Input without comments.
+ * @in: Input string containing a command line.
+ * Return: Input string without comments.
  */
-char *remove_comments(char *input)
+char *without_comment(char *in)
 {
 	int i, up_to;
 
 	up_to = 0;
-	for (i = 0; input[i]; i++)
+	for (i = 0; in[i]; i++)
 	{
-		if (input[i] == '#')
+		if (in[i] == '#') /* If a comment symbol '#' is found */
 		{
 			if (i == 0)
 			{
-				free(input);
-				return (NULL);
+				free(in);
+				return (NULL); /* If '#' is the first character, the entire input is a comment */
 			}
 
-			if (input[i - 1] == ' ' || input[i - 1] == '\t' || input[i - 1] == ';')
-				up_to = i;
+			/* If '#' is preceded by a space, tab, or semicolon, consider it as a valid comment */
+			if (in[i - 1] == ' ' || in[i - 1] == '\t' || in[i - 1] == ';')
+				up_to = i; /* Store the index up to which the input is valid (excluding the comment) */
 		}
 	}
 
 	if (up_to != 0)
 	{
-		input = _realloc(input, i, up_to + 1);
-		input[up_to] = '\0';
+		in = _realloc(in, i, up_to + 1); /* Reallocate memory to remove the comment from the input */
+		in[up_to] = '\0'; /* Null-terminate the input at the end of the valid characters */
 	}
 
-	return (input);
+	return (in); /* Return the input string without comments */
 }
 
